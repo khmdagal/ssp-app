8b34fa506393693a8a30774863a555363d22e07e
khmdagal <khmdagal@gmail.com>
2023-05-14 23:08:21 +0100

    Clean loginform

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
 import "./Login.css";
 
-export default function Login({ getUserInfoFromLogin }) {
+export default function Login() {
   const navigate = useNavigate();
   const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
-  //const [token, setToken] = useState("");
 
   async function loginUser(credentials) {
     const response = await fetch("https://spelling-server.glitch.me/login", {
@@ -25,12 +23,8 @@ export default function Login({ getUserInfoFromLogin }) {
 
       // to navigate the
       navigate("/dashboard");
-      console.log(localStorage.getItem("token"));
-
-      //setToken(data);
-      getUserInfoFromLogin(data.user); // Pass the token data back to the App component
     } else {
-      // Handle login error
+      console.log("response status is not okay! investigate");
     }
   }
 
@@ -70,72 +64,3 @@ export default function Login({ getUserInfoFromLogin }) {
     </div>
   );
 }
-
-/*
-import React, { useState } from "react";
-import "./Login.css";
-
-export default function Login(props) {
-  const [username, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  const [token, setToken] = useState();
-
-  async function loginUser(credentials) {
-    const response = await fetch("http://localhost:8080/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(credentials),
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      setToken(data);
-      props.sendDataToParent(token); // Pass the token data back to the App component
-    } else {
-      console.log("ther is error");
-    }
-
-    // .then((response) => response.json()) // Extract JSON data from the response
-    // .then((data) => setToken(data));
-  }
-  //JS LOCAL STORAGE  IN LINE 17
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    await loginUser({
-      username,
-      password,
-    });
-  };
-  if (!!token) console.log(token);
-  return (
-    <div className="login-wrapper">
-      <h1>Please Log In</h1>
-
-      <form onSubmit={handleSubmit}>
-        <label>
-          <p>Username</p>
-          <input
-            value={username}
-            type="text"
-            onChange={(e) => setUserName(e.target.value)}
-          />
-        </label>
-        <label>
-          <p>Password</p>
-          <input
-            value={password}
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
-        <div>
-          <button type="submit">Submit</button>
-        </div>
-      </form>
-    </div>
-  );
-}
-*/
