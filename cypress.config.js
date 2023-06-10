06f347e0c425c5919e8ee7c1feb2a02dbfe149a2
khmdagal <khmdagal@gmail.com>
2023-06-10 19:45:06 +0100

    Add e2e folder and modified the cypress.config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1920,
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
+    excludeSpecPattern: "",
+    excludeSpecPattern: ["**/1-getting-started/*", "**/2-advanced-examples/*"],
   },
 });
