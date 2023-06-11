2ff896d06f85d50bce21113b9ee05206e9d6788d
khmdagal <khmdagal@gmail.com>
2023-06-11 19:29:30 +0100

    First test for login

---
+++ cypress/e2e/Login.spec.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress"/>
+
+
+describe("login form", () => {
+
+  
+
+
+  it("should submit login info", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('[data-cy="userNameInputField"]').type("Khadar")
+    cy.get('[data-cy="passwordInputField"]').type("Abc123")
+    cy.get('[data-cy="submitButton"]').click();
+
+    cy.location('pathname').should('eq', '/dashboard')
+  });
+
+
+});
