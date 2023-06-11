57f06ffa24be58ef0446484bf889dd5f85965389
khmdagal <khmdagal@gmail.com>
2023-06-11 21:39:00 +0100

    test if the result contains the words

--- cypress/e2e/Login.spec.js
+++ cypress/e2e/Login.spec.js
@@ -2,18 +2,16 @@
 
 
 describe("login form", () => {
-
-  
-
-
   it("should submit login info", () => {
     cy.visit("http://localhost:3000/");
-    cy.get('[data-cy="userNameInputField"]').type("Khadar")
-    cy.get('[data-cy="passwordInputField"]').type("Abc123")
+    cy.get('[data-cy="userNameInputField"]').type("Khadar");
+    cy.get('[data-cy="passwordInputField"]').type("Abc123");
     cy.get('[data-cy="submitButton"]').click();
 
-    cy.location('pathname').should('eq', '/dashboard')
-  });
+    cy.location("pathname").should("eq", "/dashboard");
 
+    //cy.wait(10000);
+    cy.get(":nth-child(1) > .word-container > label").should("contain.text", "accident");
+  });
 
 });
