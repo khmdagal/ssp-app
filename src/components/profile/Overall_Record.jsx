b0c89d9e98c3dab65ab089ff20efd887bb3174c4
khmdagal <khmdagal@gmail.com>
2023-08-07 00:00:09 +0100

    Remove these files

--- src/components/profile/Overall_Record.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Overall_Record() {
-  // record here user all sessions record
-  
-  return <div></div>;
-}
--- src/components/profile/Overall_progress.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Overall_progress() {
-  // record here user session_accuracy_percentage field 
-  
-  return <div></div>;
-}
