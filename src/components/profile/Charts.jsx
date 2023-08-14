976c480ce30073f60901760047334d2596d17bbd
khmdagal <khmdagal@gmail.com>
2023-08-14 17:04:20 +0100

    Add chartType states

--- src/components/profile/Charts.jsx
+++ src/components/profile/Charts.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
-import { Bar } from "react-chartjs-2"
-import {Chart as ChartJS} from "chart.js/auto"
+import React, { useState } from "react";
+import { Bar, Line, Pie } from "react-chartjs-2";
+import { Charts as ChartJS } from "chart.js/auto";
+import Dropdown from "react-bootstrap/Dropdown";
 
 export default function Charts({ chartData }) {
+  const [chartType, setChartType] = useState("Line");
+
+  function selectChartType(e) {
+    const value = e.target.value;
     
-  return <Bar data={chartData} />;
+    setChartType(value);
+
+  }
+return <div></div>
 }
