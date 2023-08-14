0a06b22c610b6195e93826724ee93a6f7dae7039
khmdagal <khmdagal@gmail.com>
2023-08-14 23:34:22 +0100

    Removed Unwanted import of  Dropdown

--- src/components/profile/Charts.jsx
+++ src/components/profile/Charts.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Bar, Line, Pie } from "react-chartjs-2";
 import { Charts as ChartJS } from "chart.js/auto";
-import Dropdown from "react-bootstrap/Dropdown";
 
 export default function Charts({ chartData }) {
   const [chartType, setChartType] = useState("Line");
