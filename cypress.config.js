c21552845aa01b1472eacd09db2b5599f3b6bfe4
khmdagal <khmdagal@gmail.com>
2023-12-08 11:54:11 +0000

    Made video folder false

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
+  "video": false,
   e2e: {
     baseUrl: "http://localhost:3000",
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
